f45459e0988d8cfe1d65989086a2591732b31a97
SittaWetpa <sitta.wetpa@mail.kmutt.ac.th>
2025-04-22 17:13:31 +0700

    Delete src directory

--- src/routes/index_routes.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono";
-import { userRouter } from "./user_route.js";
-import { todoRouter } from "./todo_route.js";
-const mainRouter = new Hono();
-
-mainRouter.route("/users", userRouter);
-mainRouter.route("/todos", todoRouter);
-export { mainRouter };
